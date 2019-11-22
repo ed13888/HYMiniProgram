7f8cb0e56c378aa664660be306ce009976044e38
ed13888 <ed13888@163.com>
2019-11-22 17:47:32 +0800

    1

--- HYMiniProgram/Models/Order.cs
+++ HYMiniProgram/Models/Order.cs
@@ -12,13 +12,22 @@ namespace HYMiniProgram.Models
     {
         public string OrderNo { get; set; }
 
+        /// <summary>
+        /// 已收货
+        /// 装柜
+        /// 发船
+        /// 清关
+        /// 到达
+        /// </summary>
         public string State { get; set; }
 
         public DateTime CreateTime { get; set; }
 
-        public DateTime QSTime { get; set; }
+        public DateTime SHTime { get; set; }
 
         public DateTime ZGTime { get; set; }
+
+        public DateTime FCTime { get; set; }
 
         public DateTime QGTime { get; set; }
 
